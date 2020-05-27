9404cf63aaccf7421f7fd29b5f405ce02088ec86
guoyf <guoyf@cadg.cn>
2020-05-27 15:19:31 +0800

    Stream流和fastdfs源码

---
+++ src/main/java/study/Strem/StremTest.java
@@ -0,0 +1,23 @@
+package study.Strem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @创建人 guoyf
+ * @创建时间 2020/5/27
+ * @描述
+ */
+public class StremTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(10);
+        list.add(6);
+        list.add(7);
+        list.add(5);
+        list = list.stream().filter(v -> v >6).collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
