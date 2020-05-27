45e953f2042c61aaa175f57d5f7ae7e3380a3914
guoyf <guoyf@cadg.cn>
2020-05-27 14:51:47 +0800

    快排

--- src/main/java/study/Algorithm/QuickSort.java
+++ src/main/java/study/Algorithm/QuickSort.java
@@ -5,6 +5,12 @@ public class QuickSort {
         int arr []= new int[]{49,38,65,97,76,13,27,84};
         quickSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
+        for (int i = 1;i<=9;i++){
+            for (int j = 1;j <= i;j++){
+                System.out.print(j +" * "+ i +" = "+ (i*j)+"  ");
+            }
+            System.out.println();
+        }
     }
     /**
 
