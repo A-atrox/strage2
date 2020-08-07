bb9598613da85c6db7dda4b226fb4c7beb2b8f80
guoyf <guoyf@cadg.cn>
2020-08-07 13:54:07 +0800

    demo

--- src/main/java/smt/io/FileDemo.java
+++ src/main/java/smt/io/FileDemo.java
@@ -11,28 +11,32 @@ import java.io.IOException;
 public class FileDemo {
     /***
      * 唯一的与文件的类    关联一个文件路径，该文件可能不存在
-     * 编写路径时如果经常写\\ 或者//会显得特别麻烦  所以引入了File.separator
+     * 编写路径时如果经常写\\ 或者//会显得特别麻烦  所以引入了File.separator来适应不同的操作系统带来
+     * 的路径差异
      */
-    private static File file = new File("D:"+File.separator+"demo.txt");
-    static {
+    private static File file = new File("D:" + File.separator + "DEMO" + File.separator + "demo.txt");
 
-        if (!file.getParentFile().exists()){
+    static {// 多线程下静态只判断一次。。。可是该如何动态分配目录？
+        System.out.println("判断目录");
+        if (!file.getParentFile().exists()) {
             file.getParentFile().mkdirs();
         }
     }
+
     public static void main(String[] args) {
-       FileDemo.createOrDeleteFile(file);
+        System.out.println("静态函数");
+        FileDemo.createOrDeleteFile(FileDemo.file);
     }
-    public static void createOrDeleteFile(File file){
+
+    public static void createOrDeleteFile(File file) {
         try {
-            if(file.exists()){
+            if (file.exists()) {
                 System.out.println("文件存在，执行删除操作");
                 file.delete();
-            }else{
+            } else {
                 System.out.println("文件不存在执行创建");
                 System.out.println(file.createNewFile());
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/smt/reflec/ReflecDemo.java
@@ -0,0 +1,20 @@
+package smt.reflec;
+
+import java.lang.reflect.Constructor;
+/**
+ * @author guoyf
+ * @Date 2020/8/5
+ * @describe
+ */
+public class ReflecDemo {
+    public static void main(String[] args) {
+        try {
+            Class demo = Class.forName("QuickSort");
+            Constructor constructor = demo.getConstructor();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
