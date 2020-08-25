76d3c4a8bd42e601366ac82a31c42d0d44d91ce9
guoyf <guoyf@cadg.cn>
2020-08-25 13:56:04 +0800

    revers +

--- src/main/java/study/algorithm/IntReversal.java
+++ src/main/java/study/algorithm/IntReversal.java
@@ -8,11 +8,12 @@ package study.algorithm;
  */
 public class IntReversal {
     public static void main(String[] args) {
-        System.out.println(IntReversal(-250));
+        System.out.println(IntReversal(+250));
     }
 
     public static int IntReversal(int x) {
         char x_ = '-';
+        char xAdd = '+';
         String i = "" + x;
         StringBuilder stringBuilder = new StringBuilder();
         char[] x1 = i.toCharArray();
@@ -22,7 +23,7 @@ public class IntReversal {
         for (int j = x1.length - 1; j > 0; j--) {
             stringBuilder.append(x1[j]);
         }
-        if (x1[0] != x_) {
+        if (x1[0] != x_ && x1[0]!=xAdd) {
             stringBuilder.append(x1[0]);
         }
         return Integer.parseInt(stringBuilder.toString());
